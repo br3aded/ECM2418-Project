4bd1de1998583177d775b18003249ee434e65472
kelsey holdgate <62016085+br3aded@users.noreply.github.com>
2022-11-26 00:30:46 +0000

    Update Trait.pl

--- Trait.pl
+++ Trait.pl
@@ -12,22 +12,39 @@ divisible( N , Y )
       divisible(N,Y1).
        
     
-/*End Question 4.1
+/*End Question 4.1*/
 
-Begin Question 4.2
-possible( X, Y, Z )
-  :-
+/*Begin Question 4.2*/
+possible(X,Y,Z)
+  :- between(149,180,X),
+     between(247,270,Y),
+     between(346,360,Z).
 
-End Question 4.2
+     
+          
 
-Begin Question 4.3
-acceptable( X, Y, Z ) 
-  :-
+/*End Question 4.2*/
 
+/*Begin Question 4.3*/
 trait( X, Y, Z )
-  :-
+  :- possible( X, Y, Z ),
+     acceptable( X, Y, Z ).
 
-End Question 4.3*/
+acceptable( X, Y, Z ) 
+  :- number_chars(X,XS),
+     number_chars(Y,YS),
+     number_chars(Z,ZS),
+     append(XS,YS,XYS),
+     append(XYS,ZS,XYZS),
+     permutation(XYZS,['1','2','3','4','5','6','7','8','9']),
+     \+ prime(X),
+     \+ prime(Y),
+     \+ prime(Z),
+     100 < X,
+     X < 180,
+     180< Y,
+     Y < 270,
+     270 < Z,
+     Z < 360.
 
-main
-    :- prime(2).
+/*End Question 4.3*/
